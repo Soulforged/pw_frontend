3a4bae28b3b3f04094b278ba75a54f8f351a8082
Daniel Icardo <d.icardo@pegb.tech>
2018-04-23 14:42:59 +0400

    Updating coverage rules.

--- jest.config.js
+++ jest.config.js
@@ -42,5 +42,13 @@ module.exports = {
     "jsx",
     "node",
     "mjs"
-  ]
+  ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  }
 };
